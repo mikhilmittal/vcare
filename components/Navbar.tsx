e7f9a341f8339b60c5a45232e4fd79390fa75557
mittal <nikhilmittal16596@gmail.com>
2026-07-01 00:31:19 +0530

    Increase logo size to h-20 and update logo image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center">
-              <img src="/swasthghar.jpeg" alt="Swasthghar" className="h-14 w-auto" />
+              <img src="/swasthghar.jpeg" alt="Swasthghar" className="h-20 w-auto" />
             </div>
             
             <div className="hidden md:flex items-center space-x-8">
